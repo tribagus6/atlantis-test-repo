0682f8f1a0e41d6fbf49b200d509bd7b4bf7aa23
Tri Bagus Pamungkas <tribagus325@gmail.com>
2025-10-24 21:46:11 +0700

    adding the bucket again

--- main.tf
+++ main.tf
@@ -8,8 +8,19 @@ terraform {
 }
 
 provider "google" {
-  project = "orca-lab01" # <-- REPLACE THIS
+  project = "your-gcp-project-id" # <-- REPLACE THIS
   region  = "us-central1"
+}
+
+resource "google_storage_bucket" "atlantis_test_bucket" {
+  name     = "atlantis-test-bucket-${random_id.id.hex}"
+  location = "US"
+
+  uniform_bucket_level_access = true
+
+  labels = {
+    "managed-by" = "atlantis"
+  }
 }
 
 resource "random_id" "id" {
