6f2aee27413d14654a411f99bc720b7a34b822f3
Tri Bagus Pamungkas <tribagus325@gmail.com>
2025-10-24 22:11:42 +0700

    Destroy pub/sub

--- main.tf
+++ main.tf
@@ -11,19 +11,3 @@ provider "google" {
   project = "orca-lab01" # <-- REPLACE THIS
   region  = "us-central1"
 }
-
-# --- This is our new resource ---
-
-# We use a random_id to make sure the topic name is always unique
-resource "random_id" "topic_suffix" {
-  byte_length = 4
-}
-
-# This creates a new Pub/Sub topic
-resource "google_pubsub_topic" "atlantis_test_topic" {
-  name = "atlantis-test-topic-${random_id.topic_suffix.hex}"
-  
-  labels = {
-    "managed-by" = "atlantis"
-  }
-}
