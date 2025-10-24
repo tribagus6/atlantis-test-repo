6dc632986d73322777d4bec8988f586634efa32d
Tri Bagus Pamungkas <tribagus325@gmail.com>
2025-10-24 20:55:48 +0700

    Initial terraform project

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "your-gcp-project-id" # <-- REPLACE THIS
+  region  = "us-central1"
+}
+
+resource "google_storage_bucket" "atlantis_test_bucket" {
+  name     = "atlantis-test-bucket-${random_id.id.hex}"
+  location = "US"
+
+  uniform_bucket_level_access = true
+
+  labels = {
+    "managed-by" = "atlantis"
+  }
+}
+
+resource "random_id" "id" {
+  byte_length = 8
+}
